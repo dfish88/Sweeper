ad7dbfae4d2ae0a5b0106f4e94a05374e0612739
Danny <fisher.d88@gmail.com>
2019-04-06 17:44:50 -0600

    Added Tile class with empty skeleton

---
+++ Tile.java
@@ -0,0 +1,30 @@
+public class Tile
+{
+	private int x;
+	private int y;
+
+	public Tile()
+	{}
+
+	public Tile(int x, int y)
+	{
+	}
+
+	public void setX(int x)
+	{
+	}
+
+	public void setY(int y)
+	{
+	}
+
+	public int getX()
+	{
+		return this.x;
+	}
+
+	public int getY()
+	{
+		return this.y;
+	}
+}
