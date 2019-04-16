9dde1c8e38727e71ae1436d8623e8e7e40431261
Danny <fisher.d88@gmail.com>
2019-04-16 10:39:37 -0600

    Replaced action listener with mouse listener.  This allows us to distinguish between left and right mouse clicks.

--- java/Game.java
+++ java/Game.java
@@ -1,20 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 public class Game
 {
 	private JFrame frame;
 	private JButton buttonGrid[][];
 	private Board gameBoard;
-	private ButtonListener listener;
+	private GameListener listener;
 
 	public Game(int dimension)
 	{
 		this.gameBoard = new Board(dimension);
 
-		this.listener = new ButtonListener();
+		this.listener = new GameListener();
 		this.frame = new JFrame("Mine Sweeper!");
 		this.buttonGrid = new JButton[dimension][dimension];
+
+		this.buildWindow(dimension);
+	}
+
+	private void buildWindow(int dimension)
+	{
 
 		for (int i = 0; i < dimension; i++)
                 {
@@ -22,7 +29,7 @@ public class Game
                         {
                                 this.buttonGrid[i][j] = new JButton();
                                 this.buttonGrid[i][j].putClientProperty("coordinates", new Integer[]{i,j});
-                                this.buttonGrid[i][j].addActionListener(this.listener);
+                                this.buttonGrid[i][j].addMouseListener(this.listener);
                                 this.frame.add(buttonGrid[i][j]);
                         }
 
@@ -38,15 +45,31 @@ public class Game
 		this.buttonGrid[x][y].setText(this.gameBoard.revealTile(x,y));
 	}
 
-	private class ButtonListener implements ActionListener
+	private class GameListener implements MouseListener
 	{
-		public void actionPerformed(ActionEvent e)
+		public void mouseClicked(MouseEvent e)
 		{
 			JButton button = (JButton) e.getSource();
 			Integer[] coordinates = (Integer []) button.getClientProperty("coordinates");
-			System.out.println("Button pressed at: " + coordinates[0] + ", " + coordinates[1]);
-	
+
+			if (SwingUtilities.isLeftMouseButton(e))
+				System.out.println("Left Button pressed at: " + coordinates[0] + ", " + coordinates[1]);
+			else if (SwingUtilities.isRightMouseButton(e))
+				System.out.println("Right Button pressed at: " + coordinates[0] + ", " + coordinates[1]);
+				
 			Game.this.revealTiles(coordinates[0], coordinates[1]);
 		}
+		
+		public void mousePressed(MouseEvent e) 
+		{}
+
+		public void mouseReleased(MouseEvent e)
+		{}
+
+		public void mouseEntered(MouseEvent e)
+		{}
+
+      		public void mouseExited(MouseEvent e)
+		{}
 	}
 }
