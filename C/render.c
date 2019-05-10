65f959c515eed399e916dd3094ce14429d0f86e4
Danny <fisher.d88@gmail.com>
2019-05-10 15:42:42 -0600

    Now draws the board using SDL viewports to define each button.

--- C/render.c
+++ C/render.c
@@ -6,27 +6,31 @@
 //Screen dimension constants
 const int SCREEN_WIDTH = 400;
 const int SCREEN_HEIGHT = 400;
+const int IMAGE_SIZE = 50;
 const int ADJACENT = 9;
 
 // Images used for game
-SDL_Surface** adjacent;
-SDL_Surface* flag;
-SDL_Surface* mine;
-SDL_Surface* covered;
-SDL_Surface* boom;
-SDL_Surface* wrong;
-SDL_Surface* happy;
-SDL_Surface* surprise;
-SDL_Surface* dead;
-SDL_Surface* glasses;
+SDL_Texture** adjacent;
+SDL_Texture* flag;
+SDL_Texture* mine;
+SDL_Texture* covered;
+SDL_Texture* boom;
+SDL_Texture* wrong;
+SDL_Texture* happy;
+SDL_Texture* surprise;
+SDL_Texture* dead;
+SDL_Texture* glasses;
 
-SDL_Window* window = NULL;
-SDL_Surface* screen = NULL;
+SDL_Window* window;
+SDL_Renderer* renderer;
+int dimension;
+
+SDL_Rect** board;
 
 void load_images();
 int create_window();
 
-int init_render()
+int init_render(int d)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
 	{
@@ -38,9 +42,35 @@ int init_render()
 		return EXIT_FAILURE;
 	}
 
-	adjacent = malloc(ADJACENT * sizeof(SDL_Surface*));
+	adjacent = malloc(ADJACENT * sizeof(SDL_Texture*));
 	create_window();
 	load_images();
+
+	dimension = d;
+
+	int i, j;
+	board = malloc(dimension * sizeof(SDL_Rect*));
+	for (i = 0; i < dimension; i++)
+		board[i] = malloc(dimension * sizeof(SDL_Rect));
+
+
+	
+	int r = 0;
+	for (i = 0; i < dimension; i++)
+	{
+		for (j = 0; j < dimension; j++)
+		{
+			board[i][j].x = j * IMAGE_SIZE;
+			board[i][j].y = i * IMAGE_SIZE;
+			board[i][j].w = IMAGE_SIZE;
+			board[i][j].h = IMAGE_SIZE;
+
+			SDL_RenderSetViewport(renderer, &board[i][j]);
+
+			SDL_RenderCopy(renderer, adjacent[r], NULL, NULL);
+			r = (r + 1) % ADJACENT;
+		}
+	}
 
 	return 0;
 }
@@ -56,17 +86,33 @@ int create_window()
 	}
 	else
 	{
-		//Get window surface
-		screen = SDL_GetWindowSurface( window );
+		//Create renderer for window
+		renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+		if( renderer == NULL )
+		{
+			printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Initialize renderer color
+			//SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+			//Initialize PNG loading
+			int imgFlags = IMG_INIT_PNG;
+			if( !( IMG_Init( imgFlags ) & imgFlags ) )
+			{
+			    printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+			}
+		}
 	}
 	return 0;
 }
 
-SDL_Surface* load_optimized(char* path)
+SDL_Texture* load_optimized(char* path)
 {
 	SDL_Surface* tmp = IMG_Load(path);
 	//Convert surface to screen format
-	SDL_Surface* opt = SDL_ConvertSurface( tmp, screen->format, 0 );
+	SDL_Texture* opt = SDL_CreateTextureFromSurface(renderer, tmp);
 	SDL_FreeSurface(tmp);
 	return opt;
 }
@@ -88,12 +134,14 @@ void destroy_render()
 {
 	free(adjacent);
 
-	SDL_FreeSurface(screen);
-	screen = NULL;
-
 	//Destroy window
 	SDL_DestroyWindow( window );
 	window = NULL;
+
+	int i;
+	for (i = 0; i < dimension; i++)
+		free(board[i]);
+	free(board);
 
 	//Quit SDL subsystems
 	SDL_Quit();
@@ -101,22 +149,15 @@ void destroy_render()
 
 void make_window()
 {
-	init_render();
-	SDL_BlitSurface( adjacent[0], NULL, screen, NULL );
-	SDL_UpdateWindowSurface(window);
+	init_render(8);
+	SDL_RenderPresent(renderer);
 }
 
 int get_input()
 {
 	SDL_Event e;	
-	static int cur_img = 1;
 	while(SDL_PollEvent(&e) != 0)
 	{
-		SDL_BlitSurface( adjacent[cur_img], NULL, screen, NULL );
-		SDL_UpdateWindowSurface(window);
-		cur_img++;
-		cur_img = cur_img % ADJACENT;
-		
 		if(e.type == SDL_QUIT)
 		{
 			printf("QUIT!\n");
