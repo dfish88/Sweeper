8f7174782f27d64526ae516aeb01f6e120b5575e
Danny Fisher <fisher.d88@gmail.com>
2020-02-17 15:42:49 -0700

    Have the top row of the game displaying. Should work on first screen first.

--- python/main.py
+++ python/main.py
@@ -5,16 +5,28 @@ BUTTON_DIM = 50
 
 def main():
 
-	# Make Top bar
-
-	# Make window
-	row, col = 8, 8
+	# Create main window
 	root = tkinter.Tk()
 	root.title("Sweeper!")
 	root.grid()
+
+	# Make Top bar that has hint, smiley, restart, timer
+	hint = tkinter.Button(root, text="Hint?")
+	smile_img = tkinter.PhotoImage(file="../icons/smile.png")
+	smile = tkinter.Label(root, image=smile_img)
+	restart = tkinter.Button(root, text="Restart?")
+	timer = tkinter.Label(root, text="0:00")
+
+	hint.grid(row=0, column=0, columnspan=2, sticky="nsew")
+	smile.grid(row=0, column=3)
+	restart.grid(row=0, column=5, columnspan=2, sticky="nsew")
+	timer.grid(row=0, column=7)
+
+	# Make window
+	row, col = 8, 8
 	blank = tkinter.PhotoImage(file="../icons/blank.png")
 
-	for r in range(row):
+	for r in range(1, row+1):
 		for c in range(col):
 			callback = lambda x=r, y=c: print(str(x) + ", " + str(y))
 			temp = tkinter.Button(root, image=blank, highlightthickness=0, bd=0, relief=SUNKEN, command = callback)
