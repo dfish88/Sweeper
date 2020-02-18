fcfd08655c2bab13316b7a6fe0965b3c7b3ab010
Danny Fisher <fisher.d88@gmail.com>
2020-02-18 11:20:17 -0700

    Have starting screen with difficulty option.  Changed to using two frames instead of just root window.

--- python/main.py
+++ python/main.py
@@ -8,20 +8,47 @@ def main():
 	# Create main window
 	root = tkinter.Tk()
 	root.title("Sweeper!")
-	root.grid()
+	root.rowconfigure(0, weight=1)
+	root.columnconfigure(0, weight=1)
+
+	# Top frame has hint, smiley, restart, and timer
+	top_frame = tkinter.Frame(root, width=BUTTON_DIM*8, height=BUTTON_DIM)
+	top_frame.grid(row=0, column=0, sticky="nsew")
+	top_frame.grid_propagate(0)
+	
+	# Bottom frame has mine field
+	bottom_frame = tkinter.Frame(root, width=BUTTON_DIM*8, height=BUTTON_DIM*8)
+	bottom_frame.grid(row=1, column=0, sticky="nsew")
+	bottom_frame.grid_propagate(0)
 
 	# Make Top bar that has hint, smiley, restart, timer
-	hint = tkinter.Button(root, text="Hint?")
+	hint = tkinter.Button(top_frame, text="Hint?")
 	smile_img = tkinter.PhotoImage(file="../icons/smile.png")
-	smile = tkinter.Label(root, image=smile_img)
-	restart = tkinter.Button(root, text="Restart?")
-	timer = tkinter.Label(root, text="0:00")
+	smile = tkinter.Label(top_frame, image=smile_img)
+	restart = tkinter.Button(top_frame, text="Restart?")
+	timer = tkinter.Label(top_frame, text="0:00")
 
+	#top_frame.rowconfigure(0, weight=1)
 	hint.grid(row=0, column=0, columnspan=2, sticky="nsew")
+	top_frame.columnconfigure(0, weight=1)
 	smile.grid(row=0, column=3)
+	top_frame.columnconfigure(3, weight=1)
 	restart.grid(row=0, column=5, columnspan=2, sticky="nsew")
+	top_frame.columnconfigure(5, weight=1)
 	timer.grid(row=0, column=7)
+	top_frame.columnconfigure(7, weight=1)
 
+	# Offer two choices of difficulty
+	easy = tkinter.Button(bottom_frame, text="8x8 (Easy)")
+	hard = tkinter.Button(bottom_frame, text="16x16 (Hard)")
+
+	bottom_frame.rowconfigure(0, weight=1)
+	easy.grid(row=0, column=0, columnspan=4, rowspan=8, sticky="nsew")
+	bottom_frame.columnconfigure(0, weight=1)
+	hard.grid(row=0, column=4, columnspan=4, rowspan=8, sticky="nsew")
+	bottom_frame.columnconfigure(4, weight=1)
+
+	'''
 	# Make window
 	row, col = 8, 8
 	blank = tkinter.PhotoImage(file="../icons/blank.png")
@@ -31,6 +58,6 @@ def main():
 			callback = lambda x=r, y=c: print(str(x) + ", " + str(y))
 			temp = tkinter.Button(root, image=blank, highlightthickness=0, bd=0, relief=SUNKEN, command = callback)
 			temp.grid(row = r, column=c)
-
+	'''
 	root.mainloop()
 main()
