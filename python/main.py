e92ef0ccdc291afabbceddfeb64e64989053e592
Danny Fisher <fisher.d88@gmail.com>
2020-02-20 13:05:22 -0700

    Added function to load all icons and store them in dictionary.

--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter
 from tkinter import *
 
@@ -5,24 +6,47 @@ BUTTON_DIM = 50
 EASY_DIM = 8
 HARD_DIM = 16
 
-def start_game(size, old_frame, window):
+def start_game(size, container, img):
 	
-	old_frame.grid_forget()
-	
-	game_frame = tkinter.Frame(window, width=BUTTON_DIM*8, height=BUTTON_DIM*8)
-	game_frame.grid(row=1, column=0, sticky="nsew")
-	#game_frame.grid_propagate(0)
+	game_frame = tkinter.Frame(container, width=BUTTON_DIM*8, height=BUTTON_DIM*8)
+	game_frame.grid(row=0, column=0, sticky="nsew")
+	btn = tkinter.Button(game_frame, image=img, command=lambda: print("hi"))
+	btn.grid(row=0, column=0)
+	game_frame.tkraise()
 
 	# Make window
+	'''
 	row, col = size, size
-	blank = tkinter.PhotoImage(file="../icons/blank.png")
 
 	for r in range(row):
 		for c in range(col):
 			callback = lambda x=r, y=c: print(str(x) + ", " + str(y))
 			temp = tkinter.Button(game_frame, image=blank, highlightthickness=0, bd=0, relief=SUNKEN, command = callback)
 			temp.grid(row=r, column=c)
-	game_frame.tkraise()
+	'''
+
+# Load all the icons used during the game
+def load_icons(img_dic):
+
+	img_dic['0'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['1'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['2'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['3'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['4'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['5'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['6'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['7'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['8'] = tkinter.PhotoImage(file="../icons/0.png")
+	img_dic['b'] = tkinter.PhotoImage(file="../icons/blank.png")
+	img_dic['bm'] = tkinter.PhotoImage(file="../icons/boom.png")
+	img_dic['c'] = tkinter.PhotoImage(file="../icons/click.png")
+	img_dic['d'] = tkinter.PhotoImage(file="../icons/dead.png")
+	img_dic['e'] = tkinter.PhotoImage(file="../icons/empty.png")
+	img_dic['f'] = tkinter.PhotoImage(file="../icons/flag.png")
+	img_dic['g'] = tkinter.PhotoImage(file="../icons/glasses.png")
+	img_dic['m'] = tkinter.PhotoImage(file="../icons/mine.png")
+	img_dic['s'] = tkinter.PhotoImage(file="../icons/smile.png")
+	img_dic['w'] = tkinter.PhotoImage(file="../icons/wrong.png")
 
 def main():
 
@@ -34,11 +58,14 @@ def main():
 	# Top frame has hint, smiley, restart, and timer
 	top_frame = tkinter.Frame(root, width=BUTTON_DIM*8, height=BUTTON_DIM)
 	top_frame.grid(row=0, column=0, sticky="nsew")
-	
-	# Bottom frame has mine field
-	bottom_frame = tkinter.Frame(root, width=BUTTON_DIM*8, height=BUTTON_DIM*8)
+
+	# Bottom frame has game board frame
+	bottom_frame = tkinter.Frame(root)
 	bottom_frame.grid(row=1, column=0, sticky="nsew")
-	bottom_frame.grid_propagate(0)
+	
+	# game board frame has mine field
+	game_board = tkinter.Frame(bottom_frame, width=BUTTON_DIM*8, height=BUTTON_DIM*8)
+	game_board.grid(row=0, column=0, sticky="nsew")
 
 	# Make Top bar that has hint, smiley, restart, timer
 	hint = tkinter.Button(top_frame, text="Hint?")
@@ -56,15 +83,19 @@ def main():
 	timer.grid(row=0, column=7)
 	top_frame.columnconfigure(7, weight=1)
 
-	# Offer two choices of difficulty
-	easy = tkinter.Button(bottom_frame, text="8x8 (Easy)", command=lambda:start_game(EASY_DIM, bottom_frame, root))
-	hard = tkinter.Button(bottom_frame, text="16x16 (Hard)", command=lambda:start_game(HARD_DIM, bottom_frame, root))
+	# Load all icons for game
+	icons = {}
+	load_icons(icons)
 
-	bottom_frame.rowconfigure(0, weight=1)
+	# Offer two choices of difficulty
+	easy = tkinter.Button(game_board, text="8x8 (Easy)", command=lambda:start_game(EASY_DIM, bottom_frame, icons['b']))
+	hard = tkinter.Button(game_board, text="16x16 (Hard)", command=lambda:start_game(HARD_DIM, bottom_frame, icons['b']))
+
+	game_board.rowconfigure(0, weight=1)
 	easy.grid(row=0, column=0, columnspan=4, rowspan=8, sticky="nsew")
-	bottom_frame.columnconfigure(0, weight=1)
+	game_board.columnconfigure(0, weight=1)
 	hard.grid(row=0, column=4, columnspan=4, rowspan=8, sticky="nsew")
-	bottom_frame.columnconfigure(4, weight=1)
+	game_board.columnconfigure(4, weight=1)
 
 	root.mainloop()
 main()
