ec22a9d17415dc93e46cf4b38b49d09325164f35
Danny Fisher <fisher.d88@gmail.com>
2020-02-22 16:03:01 -0700

    Logic module now builds board on first click. Still need to reveal tiles and return changes.

--- src/python/logic.py
+++ src/python/logic.py
@@ -1,37 +1,57 @@
 import random
 import const
 
+DIRECTIONS = 8
+DELTA_X = [-1,-1,0,1,1,1,0,-1]
+DELTA_Y = [0,1,1,1,0,-1,-1,-1]
+
 first_move = True
-mines_left = 0
 
-def place_mines(x, y, board, size):
+def build_board(x, y, board, size):
 
+	# Place all the mines
 	num_mines = int((size*size) / 6)
 	for mines in range(num_mines):
 
 		# Pick mine spot randomly
-		mine_x = random.randit(0, size-1)
-		mine_y = random.randit(0, size-1)
+		mine_x = random.randint(0, size-1)
+		mine_y = random.randint(0, size-1)
 
 		# Pick new mine spot if occupied or where person clicked
 		while mine_x == x or mine_y ==y or board[mine_x][mine_y] != None:
-			mine_x = random.randit(0, size-1)
-			mine_y = random.randit(0, size-1)
+			mine_x = random.randint(0, size-1)
+			mine_y = random.randint(0, size-1)
 
 		# Place a mine
-		board[mine_x][mine_y] = Tile(x=mine_x, y=mine_y, adjacent=0, covered=True, flag=False, mine=True, symbol='m')
+		board[mine_x][mine_y] = const.Tile(x=mine_x, y=mine_y, adjacent=0, covered=True, flag=False, mine=True, symbol='m')
 
-def build_board(x, y, board, size):
+	# Fill out rest of board
+	for r in range(size):
+		for c in range(size):
 
-	place_mines(x, y, board, size)
-
-	# Fill out rest of board 
+			# Skip mines
+			if board[r][c] != None:
+				continue
+		
+			# For each tile, check each adjacent direction for mines
+			adj = 0
+			for i in range(DIRECTIONS):
+				try:
+					if board[x + DELTA_X[i]][y + DELTA_Y[i]].mine:
+						adj+=1
+				except:
+					pass
+			board[r][c] = const.Tile(x=r, y=c, adjacent=adj, covered=True, flag=False, mine=True, symbol='c')
+	
 
 def make_move(x, y, board, size):
 
+	global first_move
+	changes = []
+
 	# Build board if first move
 	if first_move:
-
-		return
 		build_board(x, y, board, size)	
+		first_move = False
 
+		
